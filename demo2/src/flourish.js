59aab53a08a0d59ba0846e76e1c3b12b95e25bd5
sandhya <sandhya@gmail.com>
2024-07-02 22:10:18 +0530

    second commit

---
+++ demo2/src/flourish.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const flourish = () => {
+  return (
+    <div>
+      <div class="flourish-embed" data-src="story/2455217"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default flourish
+
